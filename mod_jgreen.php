b9c5249a8ff184d7055a2e3226b25f66714b0710
Vanndy <Vanndy>
2015-02-19 12:25:14 +0700

    First commit Module jGreen

---
+++ mod_jgreen.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * JGreen!
+ * 
+ * @package    Khemr Joomla module
+ * @subpackage Modules
+ * @link dev.sunvanndy.com
+ * @license        GNU/GPL, see LICENSE.php
+ * mod_jgreen is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+ 
+
+// no direct access
+defined('_JEXEC') or die;
+// Include the syndicate functions only once
+require_once( dirname(__FILE__) . '/helper.php' );
+ 
+$hello = modjGreenHelper::getHello($params);
+require( JModuleHelper::getLayoutPath('mod_jgreen'));
+?>
